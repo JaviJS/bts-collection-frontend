80b4a5f92a6d0f5bf54140c9723edaea9010dc64
JaviJS <javivimi14@gmail.com>
2024-02-29 21:42:17 -0300

    'config'

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+
+import {
+  CaretRightFill,
+  CaretRightOutline,
+} from '@ant-design/icons-angular/icons';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    importProvidersFrom(
+      NzIconModule.forRoot([CaretRightFill, CaretRightOutline])
+    ),
+  ],
 };
