8bef9deaa3201c15c8688d6212424ca6622fa483
JaviJS <javivimi14@gmail.com>
2024-03-05 18:44:44 -0300

    advance

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,19 +3,22 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NZ_ICONS } from 'ng-zorro-antd/icon';
+import { IconDefinition } from '@ant-design/icons-angular';
+import * as AllIcons from '@ant-design/icons-angular/icons';
 
-import {
-  CaretRightFill,
-  CaretRightOutline,
-} from '@ant-design/icons-angular/icons';
+const antDesignIcons = AllIcons as {
+  [key: string]: IconDefinition;
+};
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key])
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    importProvidersFrom(
-      NzIconModule.forRoot([CaretRightFill, CaretRightOutline])
-    ),
+    // importProvidersFrom(
+    //   // NzIconModule.forRoot([CaretRightFill, CaretRightOutline])
+    // ),
+    { provide: NZ_ICONS, useValue: icons }
   ],
 };
--- src/app/pages/dashboard-band/dashboard-band.component.ts
+++ src/app/pages/dashboard-band/dashboard-band.component.ts
@@ -1,15 +1,51 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { DemoNgZorroAntdModule } from '../../ng-zorro-antd.module';
-import { AlbumCardComponent } from '../../components/cards/album-card/album-card.component'
+import { AlbumCardComponent } from '../../components/cards/album-card/album-card.component';
+import { ActivatedRoute } from '@angular/router';
+import { NzTabsCanDeactivateFn } from 'ng-zorro-antd/tabs';
 @Component({
-    selector: 'app-info-bts',
-    standalone: true,
-    imports: [CommonModule, RouterOutlet, DemoNgZorroAntdModule, AlbumCardComponent],
-    templateUrl: './dashboard-band.component.html',
-    styleUrl: './dashboard-band.component.scss'
+  selector: 'app-info-bts',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    RouterOutlet,
+    DemoNgZorroAntdModule,
+    AlbumCardComponent,
+  ],
+  templateUrl: './dashboard-band.component.html',
+  styleUrl: './dashboard-band.component.scss',
 })
-
 export class DashboardBandComponent {
+  routeBand: string = '';
+
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit(): void {}
+
+  canDeactivate: NzTabsCanDeactivateFn = (
+    fromIndex: number,
+    toIndex: number
+  ) => {
+    console.log(toIndex);
+    let band = this.route.snapshot.paramMap.get('band');
+    // console.log('route', this.route.snapshot.paramMap.get('band'));
+    if (band) {
+      this.routeBand = band;
+    }
+    return true;
+  };
+  initRoute() {
+    let band = this.route.snapshot.paramMap.get('band');
+    // console.log('route', this.route.snapshot.paramMap.get('band'));
+    if (band) {
+      return (this.routeBand = band);
+    } else {
+      return '';
+    }
+  }
+  onBack(): void {
+    console.log('onBack');
+  }
 }
